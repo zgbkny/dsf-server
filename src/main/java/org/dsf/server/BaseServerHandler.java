678d48da00ec2761c8191b631304bc9249126123
unknown <william.ww@MININT-811U2CE.hz.ali.com>
2015-08-31 18:51:06 +0800

    all

--- src/main/java/org/dsf/server/BaseServerHandler.java
+++ src/main/java/org/dsf/server/BaseServerHandler.java
@@ -1,13 +1,19 @@
 package org.dsf.server;
 
 import io.netty.buffer.ByteBuf;
+import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerAdapter;
 import io.netty.channel.ChannelHandlerContext;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class BaseServerHandler extends ChannelHandlerAdapter {
 	
+	private Map<Channel, Object> channelMap;
+	
 	public BaseServerHandler() {
-		
+		channelMap = new HashMap<Channel, Object>();
 	}
 	
 	
@@ -26,14 +32,20 @@ public class BaseServerHandler extends ChannelHandlerAdapter {
 	@Override  
     public void channelRead(ChannelHandlerContext ctx, Object msg)  
             throws Exception {  
+		ctx.channel();
+		
+		StringBuilder sb = (StringBuilder) getOrAddChannel(ctx.channel());
+		
+		
         System.out.println("BaseServerInHandler.channelRead");  
         ByteBuf result = (ByteBuf) msg;  
         byte[] result1 = new byte[result.readableBytes()];  
         // msg中存储的是ByteBuf类型的数据，把数据读取到byte[]中  
         result.readBytes(result1);  
         String resultStr = new String(result1);  
+        sb.append(resultStr);
         // 接收并打印客户端的信息  
-        System.out.println("Client said:" + resultStr);  
+        System.out.println("Client said:" + sb.toString());  
         // 释放资源，这行很关键  
         result.release();  
   
@@ -50,4 +62,13 @@ public class BaseServerHandler extends ChannelHandlerAdapter {
     public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {  
         ctx.flush();  
     }  
+    
+    private Object getOrAddChannel(Channel channel) {
+    	Object obj = channelMap.get(channel);
+    	if (obj == null) {
+    		obj = new StringBuilder();
+    		channelMap.put(channel, obj);
+    	}
+    	return obj;
+    }
 }
--- src/main/java/org/dsf/server/DSFServer.java
+++ src/main/java/org/dsf/server/DSFServer.java
@@ -1,5 +1,5 @@
 package org.dsf.server;
 
 public class DSFServer {
-
+	public O
 }
